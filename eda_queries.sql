a7205e0a955ae0e854ce4e385816701effff6e3e
Shruti Singh <shrutisingh110204@gmail.com>
2026-01-21 22:43:24 +0530

    EDA Queries

--- eda_queries.sql
+++ eda_queries.sql
@@ -42,6 +42,7 @@ ORDER BY 2 DESC
 LIMIT 5;
 -- now that's just on a single day
 
+
 -- Companies with the most Total Layoffs
 SELECT company, SUM(total_laid_off)
 FROM world_layoffs.layoffs_staging2
